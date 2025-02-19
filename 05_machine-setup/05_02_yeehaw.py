31109ccf253c180038e1f6f555bbf70fd758992e
Phuong Pham <kris_pham@yahoo.com>
2025-02-18 23:22:19 -0800

    added 05_machine-setup lab solution

--- 05_machine-setup/05_02_yeehaw.py
+++ 05_machine-setup/05_02_yeehaw.py
@@ -3,6 +3,6 @@
 # 	I'm a programmer now.
 # 	Yeehaw!
 # 	Coding here I come!
-print("I'am a programmer now.")
-print("Yeehaw!")
-print("Coding here I come!")
+print("""I'm a programmer now.
+      Yeehaw!
+      Coding here I come!""")
