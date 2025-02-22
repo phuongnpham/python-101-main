c11bc9f2647856148b06358a59fd932d5d8c7804
phuongntpham <kris_pham@yahoo.com>
2025-02-21 22:09:46 -0800

    added 07_variables-and-types solution

--- 07_variables-and-types/07_04_multiple_improve.py
+++ 07_variables-and-types/07_04_multiple_improve.py
@@ -1,4 +1,5 @@
 # Improve the variable assignment shown below.
-# Why should you change it?
+# Why should you change it? Because color and year is not closely related
 
-color, year = "red", 1900
+color = "red"
+year = 1900
