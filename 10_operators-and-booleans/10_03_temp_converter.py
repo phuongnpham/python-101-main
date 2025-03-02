8df7cf91f54ba4a515035ee5a7c620d702358b5e
phuongntpham <kris_pham@yahoo.com>
2025-03-01 22:27:49 -0800

    added 10_operators-and-booleans lab solution

--- 10_operators-and-booleans/10_03_temp_converter.py
+++ 10_operators-and-booleans/10_03_temp_converter.py
@@ -2,3 +2,6 @@
 # ----------------------
 # Write the necessary code to convert a degree in Fahrenheit
 # to Celsius and print it to the console. Use variable names!
+f = 1
+c = (f - 32) * (5 / 9)
+print(c)
