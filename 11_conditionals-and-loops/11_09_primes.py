11ced429dd64ee2115209fb09daf2aad8499b50b
phuongntpham <kris_pham@yahoo.com>
2025-03-06 21:40:28 -0800

    added 11_conditionals-and-loops lab solution

--- 11_conditionals-and-loops/11_09_primes.py
+++ 11_conditionals-and-loops/11_09_primes.py
@@ -1,1 +1,14 @@
 # Print out every prime number between 1 and 1000.
+for num in range(1, 1001):
+    if num < 2:
+        continue
+    
+    prime = True
+
+    for i in range(2, (num//2)+1):
+        if (num % i) == 0:
+            prime = False
+            break
+
+    if prime:
+        print(num, end=" ")
--- 11_conditionals-and-loops/11_11_fizzbuzz.py
+++ 11_conditionals-and-loops/11_11_fizzbuzz.py
@@ -4,3 +4,15 @@
 # - If the number is divisible by 3, write `Fizz` instead of the number
 # - If the number is divisible by 5, write `Buzz` instead of the number
 # - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
+
+n = 100
+
+for i in range(1, n + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz', end=' ')
+    elif i % 3 == 0:
+        print('Fizz', end=' ')
+    elif i % 5 == 0:
+        print('Buzz', end=' ')
+    else:
+        print(i, end=' ')
