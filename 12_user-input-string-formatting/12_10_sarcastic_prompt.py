7c3a4a989e319ce5fa2ce43d3daa51cf39b54969
phuongntpham <kris_pham@yahoo.com>
2025-03-11 21:32:50 -0700

    added 12_user-input-string-formatting lab solution

--- 12_user-input-string-formatting/12_10_sarcastic_prompt.py
+++ 12_user-input-string-formatting/12_10_sarcastic_prompt.py
@@ -1,2 +1,12 @@
 # Create a sarcastic program that asks a user for their honest opinion,
 # then prints the same sentence back to them in aLtErNaTiNg CaPs.
+
+user_opinion = input("Please enter your honest opinion: ")
+input_lower = user_opinion.lower()
+sarcastic_message = " "
+for i, char in enumerate(input_lower):
+    if i % 2 == 0:
+        sarcastic_message += char.lower()
+    else:
+        sarcastic_message += char.upper()
+print(sarcastic_message)
--- 12_user-input-string-formatting/12_12_cococoding.py
+++ 12_user-input-string-formatting/12_12_cococoding.py
@@ -1,3 +1,6 @@
 # Write the necessary code to display the follow message to the console
 #       Time for co-co-co-ding.
 # Use an operator and f-string formatting to create this output
+
+message = "Time for co-co-co-ding"
+print(f"{message:>28}")
