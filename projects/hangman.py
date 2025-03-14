d6ceaeb1bf9ee786117b2211f4967085eae2ec76
phuongntpham <kris_pham@yahoo.com>
2025-03-13 21:33:02 -0700

    added hangman project solution

--- projects/hangman.py
+++ projects/hangman.py
@@ -23,3 +23,33 @@
 # Display a winning message and the full word if they win
 
 # Display a losing message and quit the game if they don't make it
+
+word_to_guess = "hello"
+attemp_amount = 10
+print("Welcome to the Hangman game!")
+print(f"The word for today has {len(word_to_guess)} letter. Try to guess it! Good luck!")
+display_word = ["_"] * len(word_to_guess)
+
+while attemp_amount > 0:
+    print("Word to guess:", " ".join(display_word))
+    print(f"Attempt left {attemp_amount}")
+    user_input = input("Please enter a single alphabet letter: ").lower()
+
+    if not user_input.isalpha() or len(user_input) != 1:
+        print("Input invalid!")
+        continue
+    elif user_input in word_to_guess:
+        print("Correct guess!")
+        for i, char in enumerate(word_to_guess):
+            if char == user_input:
+                display_word[i] = char
+    else:
+        print("Wrong guess!")
+        attemp_amount -= 1
+
+    if "_" not in display_word:
+        print(f"Congratulation! You guessed the word: {word_to_guess}")
+        break
+
+if attemp_amount == 0:
+    print(f"Sorry! The correct word is {word_to_guess}. Better luck next time!")
