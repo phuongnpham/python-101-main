76cb275ab5a0610a87b0b314ffd659a3f40940f6
phuongntpham <kris_pham@yahoo.com>
2025-03-15 16:21:31 -0700

    added 13_modules-and-automation lab solution

--- 13_modules-and-automation/13_02_the_time_please.py
+++ 13_modules-and-automation/13_02_the_time_please.py
@@ -1,2 +1,8 @@
 # Use a built-in Python module to tell you the current date and time.
 # Research online, so you can print it in a readable manner.
+
+from datetime import datetime
+
+current_date_time = datetime.now()
+format_current_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+print(f"Current date and time {format_current_date_time}")
