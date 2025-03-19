d1deb713257eb0a4a7f4f011dcad1a5550ba1fc7
phuongntpham <kris_pham@yahoo.com>
2025-03-18 21:09:25 -0700

    added mover project solution

--- projects/mover.py
+++ projects/mover.py
@@ -12,3 +12,14 @@
 # Create a new path for each file
 
 # Move the screenshot there
+
+from pathlib import Path
+
+desktop = Path().home().joinpath("Desktop")
+screenshots = desktop.joinpath("screenshots")
+screenshots.mkdir(exist_ok=True)
+
+for png_file in desktop.iterdir():
+    if png_file.suffix == ".png":
+        destination = screenshots.joinpath(png_file.name)
+        png_file.replace(destination)
