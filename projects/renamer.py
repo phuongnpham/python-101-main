b289ae6900915cd558eb5a880d688f22b0570b11
phuongntpham <kris_pham@yahoo.com>
2025-03-19 21:59:48 -0700

    added renamer project solution

--- projects/renamer.py
+++ projects/renamer.py
@@ -5,3 +5,17 @@
 # Create a new directory
 
 # Move and rename all screenshots
+
+from pathlib import Path
+
+desktop = Path().home().joinpath("Desktop")
+screenshots = desktop.joinpath("screenshots")
+screenshots.mkdir(exist_ok=True)
+counter = 0
+
+for png_file in desktop.iterdir():
+    if png_file.suffix == ".png":
+        counter += 1
+        png_file_rename = f"screenshot_{counter}.png"
+        destination = screenshots.joinpath(png_file_rename)
+        png_file.rename(destination)
